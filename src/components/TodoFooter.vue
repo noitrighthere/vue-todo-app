cdc11e619b4d5ebaea65226f9b382cc29eb22188
nokh9503 <nokh9503@hanmail.net>
2022-01-11 15:32:58 +0900

    add modules

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -5,11 +5,13 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-methods: {
-  clearTodo() {
-    this.$store.commit('clearAllItems');
-    }
+  methods: {
+    ...mapMutations({
+      clearTodo: 'clearAllItems'
+    })
   }
 }
 </script>
